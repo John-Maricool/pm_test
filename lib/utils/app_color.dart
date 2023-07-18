6de66e282c31b1c64575604a90b360a2a46a57ab
John-Maricool <ebukaikenwa0902@gmail.com>
2023-07-18 11:15:38 -1100

    Done with setup

---
+++ lib/utils/app_color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color whiteColor = Colors.white;
+  static Color blackColor = Color(0xff0E0E0E);
+  static Color greyColor = Color(0xffF8F8F8);
+  static Color blueColor = Color(0xff0184D6);
+  static Color lightGrey = Color(0xffE9EBEE);
+}
+
+class AppFontFamily {
+  static String defaultFamily = "Mark-Pro";
+}
---
+++ lib/views/on_boarding.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
